eba58dd5332b6f2b273cf5a7fc572c3e340fa009
SpiritChrusher <szabokristof997@gmail.com>
2021-07-07 16:18:59 +0200

    the start of my first react-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
-function App() {
+
+const App = () => {
+	
+	const name = 'Kristóf'
+	const secname = "Szabó"
+	const X = true
+	
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppContainer">
+	<Header/>
+	  <h1>Hello {X ? 'Mr.' : 'Ms.'} {name + ' ' + secname}</h1>
     </div>
   );
 }
